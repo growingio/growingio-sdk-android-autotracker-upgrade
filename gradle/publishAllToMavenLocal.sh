9612a6c3be3207cd6000cafc67c4b7d486f5b240
shenliming <348515494@qq.com>
2021-04-23 14:05:24 +0800

    upload package to maven central

--- gradle/publishAllToMavenLocal.sh
+++ gradle/publishAllToMavenLocal.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 ./gradlew clean \
-&& ./gradlew :autotracker-upgrade-2to3-cdp:publishReleaseAgentPublicationToMavenLocal \
-&& ./gradlew clean
+&& ./gradlew :autotracker-upgrade-2to3-cdp:publishToMavenLocal
